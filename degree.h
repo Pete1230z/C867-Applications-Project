c5d7501815813775de0ab5ceb24e9d1ca004f693
Nicholas Petree <petree234@gmail.com>
2024-07-27 14:57:45 -0500

    Added full C++ functionality and libraries to VSCode

--- degree.h
+++ degree.h
@@ -1,1 +1,6 @@
-enum degreeProgram {SECURITY, NETWORK, SOFTWAR};
+/*C.  Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE. -- COMPLETED
+ 
+
+Note: This information should be included in the degree.h file.*/
+
+enum degreeProgram {SECURITY, NETWORK, SOFTWARE};
---
+++ student.h
@@ -0,0 +1,12 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+using namespace std;
+
+class Student {
+	public:
+	   void StudentInformation(int StudentID, string firstName, string lastName, string emailAddress, int age, string numberofdaystocomplete3Courses);
+};
+
+#endif // STUDENT_H
