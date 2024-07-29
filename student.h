7ffb57feb7a2f59394739c58e133d8724c09024d
Nicholas Petree <petree234@gmail.com>
2024-07-29 05:41:34 -0500

    Added an array of pointers, classRosterArray, and public void add to roster.h

--- student.h
+++ student.h
@@ -59,6 +59,7 @@ class Student {
 	   std::array<int, 3> getDaysInCourse() const;
 	   void printDaysInCourse() const;
 	   DegreeProgram getDegreeProgram() const; 
+	   void Print() const;
 
 	   private:
 	      string studentID;
@@ -68,8 +69,6 @@ class Student {
 		  int age;
 		  array<int, 3> daysInCourse;
 		  DegreeProgram degreeProgram;
-		  void printAll() const;
-
 };
 
 #endif 
